5459561c17c392e43a162499179aa5f25a50a061
dluman <dluman@allegheny.edu>
2022-02-18 13:20:01 -0500

    Reimplemented double click for mobile devices

--- docs/script/ui.js
+++ docs/script/ui.js
@@ -3,26 +3,42 @@ $(".syllabus-gui-window").draggable({
   cursor: "move",
   handle: ".syllabus-gui-window-title"
 });
+
 // Activate window closure
 $(".syllabus-gui-window-title-controls").on('click',function(){
   $(this).parent().parent().css('display','none');
 });
+
 // Single click should visually select an item
 $(".syllabus-icon-grid-entry").on('click',function(){
   var elem = $(this).find('.syllabus-icon-grid-entry-text');
   $(elem).addClass('syllabus-icon-grid-entry-selected');
   $('.syllabus-icon-grid-entry-text').not(elem).removeClass('syllabus-icon-grid-entry-selected');
 });
+
 // Double click should launch item actions
-$(".syllabus-icon-grid-entry").dblclick(function(){
+var delay = 0;
+$(".syllabus-icon-grid-entry").on('click',function(){
   var action = $(this).parent().attr('action-item');
-  if (action == "github") {
-    window.open("https://github.com/Allegheny-ComputerScience-302-S2022",'_blank');
+  if (delay == 0) {
+    // Integrate with other first click option?
+    delay = new Date().getTime();
   } else {
-    var elem = "#syllabus-"+action+"-window";
-    $(elem).css('display','block');
+    if(((new Date().getTime()) - delay) < 500){
+      // Is double click
+      if (action == "github") {
+        window.open("https://github.com/Allegheny-ComputerScience-302-S2022",'_blank');
+      } else {
+        var elem = "#syllabus-"+action+"-window";
+        $(elem).css('display','block');
+      }
+    } else {
+      // Is not yet double click
+      delay = new Date().getTime();
+    }
   }
 });
+
 // Clicking anywhere not on an entry should release all entries
 $(document).on('click',function(evt){
   console.log($(evt.target).closest(".syllabus-icon-grid-entry"));
